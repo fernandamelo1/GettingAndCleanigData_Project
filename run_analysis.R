7bdab378fe9d2c33a9fbf2e1c091f9f12ef25e4e
Fernanda Melo <fmelo@vasp.pt>
2015-12-13 22:12:14 +0000

    Project

--- run_analysis.R
+++ run_analysis.R
@@ -3,76 +3,83 @@ library(data.table)
 path<-getwd()
 path_dataset <- file.path(path, "UCI HAR Dataset")
 if (!file.exists(path_dataset)){
-    url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-    f <- 'Dataset.zip'
+    url <- "https://d396qusza40orc.cloudataframeront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    f <- 'ProjectData.zip'
     
     if (!file.exists(path)) {dir.create(path)}
     download.file(url, file.path(path, f))
-    unzip("Dataset.zip")
+    unzip("ProjectData.zip")
     
 }
 
+# 1 1.Merges the training and the test sets to create one data set
 
-df_train_set<-read.table(file.path(path_dataset,"train","X_train.txt"))
-dt_train_set<-data.table(df_train_set)
+dataframe_train_set<-read.table(file.path(path_dataset,"train","X_train.txt"))
+Data_train_set<-data.table(dataframe_train_set)
 
-df_test_set<-read.table(file.path(path_dataset,"test","X_test.txt"))
-dt_test_set<-data.table(df_test_set)
+dataframe_test_set<-read.table(file.path(path_dataset,"test","X_test.txt"))
+Data_test_set<-data.table(dataframe_test_set)
 
-dt_set <- rbind(dt_train_set, dt_test_set)
+Data_set <- rbind(Data_train_set, Data_test_set)
 
-df_subject_train<-read.table(file.path(path_dataset,"train","subject_train.txt"))
-dt_subject_train<-data.table(df_subject_train)
+dataframe_subject_train<-read.table(file.path(path_dataset,"train","subject_train.txt"))
+Data_subject_train<-data.table(dataframe_subject_train)
 
-df_subject_test<-read.table(file.path(path_dataset,"test","subject_test.txt"))
-dt_subject_test<-data.table(df_subject_test)
+dataframe_subject_test<-read.table(file.path(path_dataset,"test","subject_test.txt"))
+Data_subject_test<-data.table(dataframe_subject_test)
 
-dt_subject <- rbind(dt_subject_train, dt_subject_test)
-setnames(dt_subject, names(dt_subject), c("subject_id"))
+Data_subject <- rbind(Data_subject_train, Data_subject_test)
+setnames(Data_subject, names(Data_subject), c("subject_id"))
 
-df_label_train <-read.table(file.path(path_dataset,"train","y_train.txt"))
-dt_label_train <-data.table(df_label_train)
+dataframe_label_train <-read.table(file.path(path_dataset,"train","y_train.txt"))
+Data_label_train <-data.table(dataframe_label_train)
 
-df_label_test <-read.table(file.path(path_dataset,"test","y_test.txt"))
-dt_label_test <-data.table(df_label_test)
+dataframe_label_test <-read.table(file.path(path_dataset,"test","y_test.txt"))
+Data_label_test <-data.table(dataframe_label_test)
 
-dt_label <- rbind(dt_label_train, dt_label_test)
+Data_label <- rbind(Data_label_train, Data_label_test)
 
-setnames(dt_label, names(dt_label), c("activity_id"))
+setnames(Data_label, names(Data_label), c("activity_id"))
 
-dt_set<-cbind(dt_set,dt_subject, dt_label)
+Data_set<-cbind(Data_set,Data_subject, Data_label)
 
-# 2.
-df_features <- read.table(file.path(path_dataset,"features.txt"))
-dt_features<-data.table(df_features)
+# 2.Extracts only the measurements on the mean and standard deviation for each measurement
 
-setnames(dt_features, names(dt_features), c("feature_id", "feature_name"))
+dataframe_features <- read.table(file.path(path_dataset,"features.txt"))
+Data_features<-data.table(dataframe_features)
 
-my_logical_features <- grep ("mean\\(\\)|std\\(\\)", dt_features$feature_name)
+setnames(Data_features, names(Data_features), c("feature_id", "feature_name"))
 
-dt_set_mean_std <- dt_set[,my_logical_features,with=FALSE]
-dt_set_mean_std$subject_id <- dt_set$subject_id
-dt_set_mean_std$activity_id <- dt_set$activity_id
+my_logical_features <- grep ("mean\\(\\)|std\\(\\)", Data_features$feature_name)
 
-# 3. 
-df_activity_labels <- read.table(file.path(path_dataset,"activity_labels.txt"))
-dt_activity_labels <- data.table(df_activity_labels)
-setnames(dt_activity_labels, names(dt_activity_labels), c("activity_id", "activity_name"))
-dt_set_mean_std <- merge(dt_set_mean_std, dt_activity_labels, by="activity_id", all.x=TRUE)
+Data_set_mean_std <- Data_set[,my_logical_features,with=FALSE]
+Data_set_mean_std$subject_id <- Data_set$subject_id
+Data_set_mean_std$activity_id <- Data_set$activity_id
 
-order_column<-names(dt_set_mean_std)[c(2:dim(dt_set_mean_std)[2],1)]
-setcolorder(dt_set_mean_std, order_column)
+
+# 3.Uses descriptive activity names to name the activities in the data set
+
+dataframe_activity_labels <- read.table(file.path(path_dataset,"activity_labels.txt"))
+Data_activity_labels <- data.table(dataframe_activity_labels)
+setnames(Data_activity_labels, names(Data_activity_labels), c("activity_id", "activity_name"))
+Data_set_mean_std <- merge(Data_set_mean_std, Data_activity_labels, by="activity_id", all.x=TRUE)
+
+order_column<-names(Data_set_mean_std)[c(2:dim(Data_set_mean_std)[2],1)]
+setcolorder(Data_set_mean_std, order_column)
+
 
 # 4. Appropriately labels the data set with descriptive variable names. 
-dt_features_logical <- dt_features[my_logical_features,]
-setnames(dt_set_mean_std, names(dt_set_mean_std)[1:dim(dt_features_logical)[1]], as.character(dt_features_logical$feature_name))
+Data_features_logical <- Data_features[my_logical_features,]
+setnames(Data_set_mean_std, names(Data_set_mean_std)[1:dim(Data_features_logical)[1]], as.character(Data_features_logical$feature_name))
 
-# 5.
-column_selected <- 1:((dim(dt_set_mean_std)[2])-3)
-dt_tidy<-aggregate(dt_set_mean_std[,column_selected,with=FALSE],
-                   (list(dt_set_mean_std$activity_name, dt_set_mean_std$subject_id)),mean)
 
-setnames(dt_tidy, names(dt_tidy)[1:2], c("Activity_Name", "Subject_Id"))
+
+# 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+column_selected <- 1:((dim(Data_set_mean_std)[2])-3)
+Data_tidy<-aggregate(Data_set_mean_std[,column_selected,with=FALSE],
+                   (list(Data_set_mean_std$activity_name, Data_set_mean_std$subject_id)),mean)
+
+setnames(Data_tidy, names(Data_tidy)[1:2], c("Activity_Name", "Subject_Id"))
 
 f <- file.path(path, "tidy_data.txt")
-write.table(dt_tidy, f, quote = FALSE, sep = "\t", row.names = FALSE)
+write.table(Data_tidy, f, quote = FALSE, sep = "\t", row.names = FALSE)
